eb53bb4e54864e62b4b018dc83d361331bdd885b
Sh Cyiza <shcyiza@Shs-MacBook-Pro.local>
2019-08-14 00:34:28 +0200

    feat remove home page cause unusefull

--- clientform/src/router.js
+++ clientform/src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home'
 import CarwashForm from './views/CarwashForm'
 import Login from './views/Login'
 
@@ -12,11 +11,6 @@ export default new Router({
   },
   mode: 'history',
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
     {
       path: '/carwash',
       name: 'carwash',
