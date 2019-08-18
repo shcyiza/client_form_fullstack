35c5e9d0142da2eb06378076986ef40b891718d0
shcyiza <patick.cyiza@gmail.com>
2019-08-18 14:19:13 +0200

    linter fix app.js

--- carjo_api/src/app.js
+++ carjo_api/src/app.js
@@ -8,20 +8,14 @@ const apolloConfig = require("./config/apolloConfig");
 const security = require("./config/securityConfig");
 const requestLog = require("./config/requestLoggingConfig");
 
-
 mongoConfig.connect();
 const redis = redisConfig.connect();
 
 const app = express();
 
-app.use(
-    requestLog.getMiddleware(),
-    cors({origin: "*"}),
-);
-
+app.use(requestLog.getMiddleware(), cors({origin: "*"}));
 
 const apollo = apolloConfig.connect(app, redis);
-
 
 apollo.session_management.applyMiddleware(app);
 // Secure the client_form with JWT
@@ -29,12 +23,12 @@ apollo.session_management.applyMiddleware(app);
 app.post(
     apollo.client_form.path,
     security.jwtFilter,
-    security.authenticationErrorFilter(apollo.client_form.path)
+    security.authenticationErrorFilter(apollo.client_form.path),
 );
 apollo.client_form.applyMiddleware(app);
 
-
 const connection_port = 6060;
+
 app.listen(connection_port, () => {
     logger.info(
         `🚀 (Express + Apollo + Mongoose) app running at localhost:${connection_port}`,
