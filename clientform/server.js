b24106a11b154c5fc6d64447c0575a6022f9f8cb
shcyiza <patick.cyiza@gmail.com>
2019-10-22 00:19:34 +0200

    hotfix/prod CORS fix

--- clientform/server.js
+++ clientform/server.js
@@ -1,21 +1,15 @@
 const express = require('express');
-const cors = require('cors');
 const serveStatic = require('serve-static');
 
 const app = express();
 
-const whitelist = ['http://localhost:6060'];
-const corsOptions = {
-    origin(origin, callback) {
-        if (whitelist.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
+app.use(serveStatic(`${__dirname}/dist`, {
+    setHeaders: function setHeaders(res) {
+        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:6060');
+        res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     },
-};
-
-app.use(serveStatic(`${__dirname}/dist`, cors(corsOptions)));
+}));
 const port = process.env.PORT || 5000;
 app.listen(port);
-console.log(`server started ${port}`);
+console.log(`server started cors ok? ${port}`);
