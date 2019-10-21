d706998e79d6138e6ff08ce540425d333f2edec6
shcyiza <patick.cyiza@gmail.com>
2019-10-21 23:51:41 +0200

    hotfix/prod CORS fix

--- clientform/server.js
+++ clientform/server.js
@@ -1,9 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const serveStatic = require('serve-static');
 
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
-app = express();
-app.use(serveStatic(__dirname + "/dist"));
-var port = process.env.PORT || 5000;
+const app = express();
+
+const whitelist = ['http://localhost:6060'];
+const corsOptions = {
+    origin(origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+};
+
+app.use(serveStatic(`${__dirname}/dist`, cors(corsOptions)));
+const port = process.env.PORT || 5000;
 app.listen(port);
-console.log('server started '+ port);
+console.log(`server started ${port}`);
