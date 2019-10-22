41eef4cf01d69c2e0cc8a0b0d21718eceb635fa8
shcyiza <patick.cyiza@gmail.com>
2019-10-22 02:09:22 +0200

    cors fixed fixing routing

--- clientform/server.js
+++ clientform/server.js
@@ -3,13 +3,16 @@ const serveStatic = require('serve-static');
 
 const app = express();
 
-app.use(serveStatic(`${__dirname}/dist`, {
-    setHeaders: function setHeaders(res) {
-        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:6060');
-        res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-        res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    },
-}));
+app.use(serveStatic(`${__dirname}/dist`));
+
+app.get('/order_form', serveStatic(`${__dirname}/dist`));
+app.get('/checkout_order', serveStatic(`${__dirname}/dist`));
+app.get('/order_confirmed', serveStatic(`${__dirname}/dist`));
+app.get('/admin_login', serveStatic(`${__dirname}/dist`));
+app.get('/admin/offers', serveStatic(`${__dirname}/dist`));
+app.get('/admin/orders', serveStatic(`${__dirname}/dist`));
+app.get('/admin/companies', serveStatic(`${__dirname}/dist`));
+
 const port = process.env.PORT || 5000;
 app.listen(port);
 console.log(`server started cors ok? ${port}`);
