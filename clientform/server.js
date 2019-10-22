48ad9e19a953ffbfb675573077b9fab889703216
shcyiza <patick.cyiza@gmail.com>
2019-10-22 02:29:38 +0200

    cors fixed fixing routing

--- clientform/server.js
+++ clientform/server.js
@@ -3,15 +3,7 @@ const serveStatic = require('serve-static');
 
 const app = express();
 
-app.use(serveStatic(`${__dirname}/dist`));
-
-app.get('/order_form', serveStatic(`${__dirname}/dist`));
-app.get('/checkout_order', serveStatic(`${__dirname}/dist`));
-app.get('/order_confirmed', serveStatic(`${__dirname}/dist`));
-app.get('/admin_login', serveStatic(`${__dirname}/dist`));
-app.get('/admin/offers', serveStatic(`${__dirname}/dist`));
-app.get('/admin/orders', serveStatic(`${__dirname}/dist`));
-app.get('/admin/companies', serveStatic(`${__dirname}/dist`));
+app.use('*', serveStatic(`${__dirname}/dist`));
 
 const port = process.env.PORT || 5000;
 app.listen(port);
