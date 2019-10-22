0aff079ba4e5459ec31648ac3c78db88e7071abc
shcyiza <patick.cyiza@gmail.com>
2019-10-22 03:54:26 +0200

    last call

--- clientform/server.js
+++ clientform/server.js
@@ -1,9 +1,17 @@
 const express = require('express');
-const path = require('path');
 const serveStatic = require('serve-static');
+const path = require('path');
 
 const app = express();
-app.use(serveStatic(`${__dirname}/dist`));
-const port = process.env.PORT || 5000;
+
+// here we are configuring dist to serve app files
+app.use('/', serveStatic(path.join(__dirname, '/dist')));
+
+// this * route is to serve project on different page routes except root `/`
+app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, '/dist/index.html'));
+});
+
+const port = process.env.PORT || 8080;
 app.listen(port);
-console.log(`server started ${port}`);
+console.log(`app is listening on port: ${port}`);
