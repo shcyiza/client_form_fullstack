b85b25f9346e8acf319d7052e71db448e3e19fd1
shcyiza <patick.cyiza@gmail.com>
2019-10-22 03:59:27 +0200

    last call my bad

--- clientform/server.js
+++ clientform/server.js
@@ -12,6 +12,6 @@ app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, '/dist/index.html'));
 });
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5000;
 app.listen(port);
 console.log(`app is listening on port: ${port}`);
