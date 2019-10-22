b38f58960b74909b969f671fd1f27a1266c0bd23
shcyiza <patick.cyiza@gmail.com>
2019-10-22 03:16:13 +0200

    reverting file for mvp

--- clientform/server.js
+++ clientform/server.js
@@ -1,15 +1,9 @@
 const express = require('express');
+const path = require('path');
 const serveStatic = require('serve-static');
 
 const app = express();
-
-app.use(serveStatic(`${__dirname}/dist`, {
-    setHeaders: function setHeaders(res) {
-        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:6060');
-        res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-        res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    },
-}));
+app.use(serveStatic(`${__dirname}/dist`));
 const port = process.env.PORT || 5000;
 app.listen(port);
-console.log(`server started cors ok? ${port}`);
+console.log(`server started ${port}`);
