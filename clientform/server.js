a1d0cdc5aa92043542e25091b6ccf10339852b1f
shcyiza <patick.cyiza@gmail.com>
2019-10-22 02:52:23 +0200

    reverting file

--- clientform/server.js
+++ clientform/server.js
@@ -10,15 +10,6 @@ app.use(serveStatic(`${__dirname}/dist`, {
         res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     },
 }));
-
-app.get('/order_form', serveStatic(`${__dirname}/dist`));
-app.get('/checkout_order', serveStatic(`${__dirname}/dist`));
-app.get('/order_confirmed', serveStatic(`${__dirname}/dist`));
-app.get('/admin_login', serveStatic(`${__dirname}/dist`));
-app.get('/admin/offers', serveStatic(`${__dirname}/dist`));
-app.get('/admin/orders', serveStatic(`${__dirname}/dist`));
-app.get('/admin/companies', serveStatic(`${__dirname}/dist`));
-
 const port = process.env.PORT || 5000;
 app.listen(port);
 console.log(`server started cors ok? ${port}`);
